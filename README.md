81cfc6694b34aee814ff3506a049ab90ac2003fc
TCYRUS <tcyrus@users.noreply.github.com>
2015-06-30 16:13:15 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Atom [apm](https://github.com/atom/apm) Project Badges
+
+Based on the SVG template in [nodei.co](https://nodei.co/)
+
+This nodejs app generates Atom Project Badges for any project on [apm](https://github.com/atom/apm).
+
+## Link
+
+TODO
+
+## Example
+
+[![apm install package-generator](package-generator.svg)](https://atom.io/packages/package-generator)
---
+++ app.js
@@ -0,0 +1,35 @@
+var app=require('express')(),
+  fs=require('fs'),
+  request=require('request'),
+  swig=require('swig');
+
+app.set('port',(process.env.PORT||5000));
+swig.setDefaults({loader:swig.loaders.fs(__dirname+"/views")});
+
+app.get('logo.svg', function(req,res) {
+  res.sendFile('views/atom-logo.svg');
+});
+
+app.get('/apm/:name.svg', function(req,res) {
+  request("https://atom.io/api/packages/"+req.params.name, function(err,response,body) {
+    if (err) {
+      console.log('Error: %s',err);
+      res.status(500).send(err);
+    } else {
+      json=JSON.parse(body);
+      if ('message' in json) {
+        console.log('Package %s Gives Error %s',req.params.name,json['message']);
+        res.status(500).send(json['message']);
+      } else {
+        res.writeHead(200,{"Content-Type":"image/svg+xml"});
+        var badge=swig.renderFile('apm-badge.svg',json);
+        res.write(badge);
+        res.end();
+      }
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:"+app.get('port'))
+})
