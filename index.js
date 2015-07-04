aa28f3c63070b1ab5fd9544fccd93130b92410b1
TCYRUS <tcyrus@users.noreply.github.com>
2015-07-04 01:59:26 +0000

    Minor Updates and Bug Fixes

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ var app=require('express')(),
 
 app.set('port',(process.env.PORT||5000));
 app.engine('svg',swig.renderFile);
-app.set('view engine','svg');
-app.set('views',path.join(__dirname,'/views'));
+app.set('views',path.join(__dirname,'/views/themes'));
 app.set('view cache',false);
 
+var themes=["one-light","one-dark","solarized-light","solarized-dark"];
+
 app.get("/apm", function(req,res) {
-  res.sendFile(path.join(__dirname,"/views","index.html"));
+  res.sendFile(path.join(__dirname,"/views/index.html"));
 });
 
 app.get('/apm/:name.svg', function(req,res) {
-  var name = req.params.name.replace(/[|&;$%@"<>()+,]/g, "");
+  var name=req.params.name.replace(/[|&;$%@"<>()+,]/g,"");
   request("https://atom.io/api/packages/"+name, function(err,response,body) {
     if (err) {
       console.log('Error: %s',err);
@@ -25,12 +26,13 @@ app.get('/apm/:name.svg', function(req,res) {
         console.log('Package %s Gives Error %s',name,json_res['message']);
         res.status(500).send(json_res['message']);
       } else {
-        res.append("Content-Type","image/svg+xml");
+        res.append('Content-Type','image/svg+xml');
+        res.append('Cache-Control','no-cache, no-store');
         var theme='one-light';
-        if ('theme' in req.query && req.query['theme']!='icon' && req.query['theme']!='base') {
+        if ('theme' in req.query && themes.indexOf(req.query.theme)>-1) {
           theme=req.query.theme;
         }
-        res.render(theme,{json:json_res});
+        res.render(theme+".svg",{json:json_res});
       }
     }
   });
